89e748980ba037da10b02748d791cbc60ce1b088
arexma <maalexander2@yahoo.com>
2020-11-14 22:42:59 -0800

    workingproduct

--- app.py
+++ app.py
@@ -9,10 +9,20 @@ from dotenv import load_dotenv
 import urllib.parse
 import json 
 import requests
+import urllib
+from oauthlib.oauth2 import BackendApplicationClient
+from requests.auth import HTTPBasicAuth
+from requests_oauthlib import OAuth2Session
 
-SEARCH_URL = 'https://api.spotify.com/v1/search'
-FEATURES_URL = 'https://api.spotify.com/v1/audio-features'
+BASIC_URL = "https://accounts.spotify.com/api/token/"
+SEARCH_URL = 'https://api.spotify.com/v1/search/?'
+FEATURES_URL = 'https://api.spotify.com/v1/audio-features/'
+client_id = "feb48f6bfe094749afc4f4c8af0d4c74"
+client_secret = "140e93df2ab643b8a3ac6b8567c2696f"
 
+client = BackendApplicationClient(client_id=client_id)
+oauth = OAuth2Session(client=client)
+token = oauth.fetch_token(token_url=BASIC_URL, client_id=client_id, client_secret=client_secret)
 
 # Load Environment variables
 load_dotenv()
@@ -20,9 +30,6 @@ load_dotenv()
 app = Flask(__name__)
 # Allow cross domain apps to access API
 CORS(app)
-
-
-
 
 # Provide Mongo Atlas URI, stored in config file
 # app.config["MONGO_URI"] = os.getenv("MONGO_URI_MASTER")
@@ -33,8 +40,7 @@ CORS(app)
 
 # api.add_resource(User, "/user")
 
-if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+
 
 
 # Vanilla Flask route
@@ -50,23 +56,52 @@ def handle_error(err):
     messages = err.data.get("messages", ["Invalid request."])
     #return json.jsonify({"errors": messages}, err.code)
 
+"""
 def url_search_builder(name):
     query_parameters = [('q', 'f{name}')]
     encode_parameter = urllib.parse.urlencode(query_parameters)
     return f'{SEARCH_URL}&{encode_parameter}'
+"""
+
+
 
 def download_data(url):
-    response = requests.get(url)
+    head = {"Authorization": "Bearer {}".format(token["access_token"])}
+    response = requests.get(url, headers=head)
     try:
         json_text = response.json()
         return json_text
     except:
         raise
-        
-
-def spotify_id_request(name):
-    url = url_search_builder(name)
-    song_details = download_data(url)
 
 
+
+def spotify_id_request(name, track, limit):
+    get_request_url = {"q": name, "type": track, "limit": limit}
+    get_request_url_formatted = SEARCH_URL + urllib.parse.urlencode(get_request_url)
+    song_list = download_data(get_request_url_formatted)
+    songs = []
+    for item in song_list["tracks"]["items"]:
+        artist_name = item["album"]["artists"][0]["name"]
+        song_name = item["name"]
+        song_id = item["id"]
+        songs.append((artist_name, song_name, song_id))
+    return songs
+
+def spotify_song_features(song_id):
+    song_info = download_data(FEATURES_URL + song_id)
+    song_tempo = song_info["tempo"]
+    song_time_signature = song_info["time_signature"]
+    return song_tempo, song_time_signature
+
+
+
+
+# name, id, artists (name), 
+
+if __name__ == "__main__":
+    #app.run(debug=True, port=8080)
+    print(spotify_id_request("athousandyears", "track", "5"))
+    print(spotify_song_features("6lanRgr6wXibZr8KgzXxBl"))
+    
 #url_search_builder('heartless')
